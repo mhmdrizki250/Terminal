37cc8c8907e4718efeceeba3db2d8d79376f3b78
mhmdrizki250 <mhmdrizki250@gmail.com>
2024-02-03 21:18:47 +0700

    Test01

---
+++ lingkaran.js
@@ -0,0 +1,21 @@
+// Jangan sentuh code disini
+function TentukanLuasDanKelilingBelahKetupat(s, d1, d2) {
+ 
+    // TODO: buat logic dibawah sini dimana sisinya adalah 5, diagonal 1-nya adalah 8, dan diagonal 2-nya adalah 7
+  
+    var luas = 0.5 * d1 *d2;
+    var kel = 4 *s;
+  
+    console.log("Sisi belah ketupat:", s, "cm");
+    console.log("Diagonal 1 belah ketupat:", d1, "cm");
+    console.log("Diagonal 2 belah ketupat:", d2, "cm")
+  
+    console.log("Jadi luas belah ketupat : ",luas, "dan kelilingnya : ",kel);
+    // TODO: buatlah logic untuk menghitung luas dan keliling dibawah
+    // TODO: gunakan console.log untuk mencetak hasil luas dan keliling
+    
+    // Akhir pembuatan code
+  
+  }
+  
+  TentukanLuasDanKelilingBelahKetupat(5, 8, 7);
